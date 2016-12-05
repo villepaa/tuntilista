97fa103a2fa6973228b24a76291c36eb1670f9c4
Ville Paavola <villepaa@dhcp-asv-87.helsinki.fi>
2016-12-05 15:08:26 +0200

    pom.xml korjaus

--- src/main/java/wad/config/ProductionProfile.java
+++ src/main/java/wad/config/ProductionProfile.java
@@ -1,14 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package wad.config;
 
-/**
- *
- * @author villepaa
- */
+import java.net.URI;
+import java.net.URISyntaxException;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("production")
 public class ProductionProfile {
     
+    
+    
+    @Bean
+    public BasicDataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("postgresql-silhouetted-32777L"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+    }
 }
